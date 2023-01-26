382198435ba15d23ec5dbe9ead0ceea5fd647c9e
nikolas <nikolas.costa@alphasquad.com>
2023-01-26 19:30:31 -0300

    continuando o desenvolvimento do projeto final

--- ProjetoFinalNode/routes/index.js
+++ ProjetoFinalNode/routes/index.js
@@ -8,10 +8,34 @@ router.get('/', (req, res) => {
     res.render('home', obj)
 });
 
-router.get('/rota1', (req, res) => {
-    let obj = {
-        nome: req.query.nome
-    };
-    res.render('home2', obj)
+router.get('/rota2', (req, res) => {
+    let horarios = {
+        segunda: {
+            disciplina: "Gestão Agil de Proj.",
+            hora: "7:40 até 11:20"
+        },
+        terca: {
+            disciplina: "BD não relacional",
+            hora: "7:40 até 11:20"
+        },
+        quarta: {
+            disciplina: "Dev. Web III",
+            hora: "7:40 até 11:20"
+        },
+        quinta: {
+            disciplina: "Álgebra Linear",
+            hora: "7:40 até 11:20"
+        },
+        sexta: {
+            disciplina: "Técnicas de Programação II",
+            hora: "7:40 até 11:20"
+        },
+    }
+    res.render('paginaSecundaria', horarios)
+});
+
+router.get('/rota3', (req, res) => {
+
+    res.render('home2')
 });
 module.exports = router;
--- ProjetoFinalNode/server.js
+++ ProjetoFinalNode/server.js
@@ -14,5 +14,5 @@ require('dotenv').config({path:'variables.env'});
 
 app.set('port', process.env.PORT || 7777)
 const server = app.listen(app.get('port'), () => {
-    console.log("Servidor rodando na porta: " + server.address().port);
+    console.log("Servidor rodando em: localhost:" +server.address().port);
 });
